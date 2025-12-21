6cd48a5e5b7477a33f9115dc887f30343b0f52bf
Shaked Flam <shaked.flam@post.runi.ac.il>
2025-12-21 18:52:45 +0200

    Creating react template and add Header funcion

--- web/frontend/src/App.jsx
+++ web/frontend/src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Header from './components/Header'
 import './App.css'
-// return what that appear on the screen
+
+// return what that appears on the screen
 function App() {
-  const [count, setCount] = useState(0)
 
   // what that will be displayed (UI) AND update automaticaly changes
   return (
-    <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1><Header></Header></h1>
+      <p>ברוכים הבאים</p>
       </div>
-      <h1>הקליניקה של רוני</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
---
+++ web/frontend/src/components/Header.jsx
@@ -0,0 +1,8 @@
+function Header(){
+    return(
+        <header>
+            <h1>הקליניקה של רוני</h1>
+        </header>
+    )
+}
+export default Header
